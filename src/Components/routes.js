8770fb1a8a3b39d090af55479dd5f1f28680ba13
HS <HS@a218s-MacBook-Pro.local>
2019-06-07 16:22:56 -0600

    needs a little more work

--- src/Components/routes.js
+++ src/Components/routes.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Switch,  Route } from 'react-router-dom'
-import Dashboard from './Components/Dashboard/Dashboard'
-import Header from './Components/Header/Header'
-import Form from './Components/ Form/Form'
-import List from '../../List/List'
-import Product from './Components/Product/Product';
+import Dashboard from './Dashboard/Dashboard'
+import Header from './Header/Header'
+import Form from './ Form/Form'
+import List from '../List/List'
+import Product from './Product/Product';
 
 
 export default (
 <Switch>
     <Route exact path="/" component={Header} />
-    <Route exact path="/list" component={List} />
-    <Route exact path="/form" component={Form} />
+    <Route path="/list" component={List} />
+    <Route path="/form" component={Form} />
     <Route path="/dashboard" component={Dashboard} />
     <Route path="/message/:id" component={Product} />
 
